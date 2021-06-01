776f136c5fad6e4b67a89944ad2bb95562b7c342
your step dad <74175556+glacier-tm@users.noreply.github.com>
2021-06-01 17:54:23 +0300

    Update hsb_color_picker.lua

--- hsb_color_picker.lua
+++ hsb_color_picker.lua
@@ -55,38 +55,43 @@ do
 	end;
 end;
 
---calculate hsv
-
-do --uses hsb model
+do --uses hsv model
+	--what we need to do: get the hue and saturation from the circle using our data in this problem.
 	local cabsP = cursorPicker.AbsolutePosition;
 	local cabsS = cursorPicker.AbsoluteSize;
 	
+	--lets pretend cX, cY are our current input position
 	local cX, cY = cabsP.X, cabsP.Y; --cabsP.X + (cabsS.X / 2), cabsP.Y + (cabsS.Y / 2);
 
+	-- convert the position plot into element's space (relative from the viewport) and round it.
 	local x = floor((cX - pX) + 0.5);
 	local y = floor((cY - pY) + 0.5);
 
+	-- a new plot, lets make a new triangle across the diameter of the circle ( makes it a 90 degree because arcs to diameter, simple circle theorems. )
 	local drx, dry = cR - x, cR - y;
 	
-	local r = sqrt(drx * drx + dry * dry);
-	local alpha = atan2(dry, drx); --reversed params (atan2)
+	local r = sqrt(drx * drx + dry * dry); -- drx^2 + dry^2 = r^2 ( find radius using pytahgoras )
+	local alpha = atan2(dry, drx); -- find the positive angle when we got (drx, dry) ( used for hue later )
 
-	if (alpha < 0) then
+	if (alpha < 0) then -- since atan2 domain is 0 < res < 2pi we add 2pi, we dont need a while loop here since wont be really big
 		alpha = alpha + (2 * math.pi);
 	end;
 
-	if (r > cR) then --clamp
+	-- radius normalization
+	if (r > cR) then --radius is over the normal circle's radius?
 		r = cR;
+		-- lets pretend we have a unit circle inside, x is cose(theta), y is sine(theta), h/r is 1 since is unit.
+		-- we use trigo identifies then we normalize the circle using multiplying our normal radius with the result.
 		x = (r * (1 - cose(alpha)));
 		y = (r * (1 - sine(alpha)));
 	end;
 
-	--for later use
+	-- (transformation) round x, y plot
 	x = floor(x + 0.5);
 	y = floor(y + 0.5);
 	
-	local h = mapRelativeHue(alpha);
-	local s = floor((r / cR) * 255);
+	local h = mapRelativeHue(alpha); -- map to relative hue using our algorithm
+	local s = floor((r / cR) * 255); -- convert to 255%'s to round s to 255 later on
 	
 	--result : h, s, constantB
 	script.Parent.Frame.BackgroundColor3 = Color3.fromHSV(h / 360, s / 255, constantB / 255);
